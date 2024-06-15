62d3a6da91c7c5ebb386cb4fb1395dfc4876f3d3
Hongwei <gmhwxi@gmail.com>
2024-06-15 15:05:37 -0400

    Updating: very very minorly

--- assigns/04/Testing/Python/assign04_07_test.py
+++ assigns/04/Testing/Python/assign04_07_test.py
@@ -1,9 +1,17 @@
 ######################################################
 import sys
-sys.setrecursionlimit(16000)
 sys.path.append('./../../MySolution/Python')
-######################################################
 from assign04_07 import *
 ######################################################
-
-#### end of [CS320-2024-Sum1-assign04_06_test.py] ####
+# stream_iforall\
+#     (doublet_stream_from('water'), \
+#      lambda i, x: i < 1000 and not(print(i,":",x)))
+######################################################
+assert\
+    (len(stream_get_at(doublet_stream_from('water'), 672))==5)
+assert\
+    (len(stream_get_at(doublet_stream_from('water'), 673))==6)
+######################################################
+print("Assign04-07-test passed!")
+######################################################
+#### end of [CS320-2024-Sum1-assign04_07_test.py] ####
